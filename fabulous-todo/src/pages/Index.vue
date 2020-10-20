e1f9ee4da4f3f842d3989f66dace2fefe034e50c
abeer <abeer@dataloop.ai>
2020-10-20 21:40:18 +0300

    click event

--- fabulous-todo/src/pages/Index.vue
+++ fabulous-todo/src/pages/Index.vue
@@ -1,14 +1,39 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page >
+    <input
+      v-model="message" 
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage"/>
+    <button @click="clearMessage">Clear</button>
+      <h5> {{message}} </h5>
   </q-page>
 </template>
 
+
+
 <script>
 export default {
-  name: 'PageIndex'
+  data(){
+    return{
+      message : 'I love VUE.JS!'
+    }
+  },
+  methods: {
+     clearMessage(){
+       this.message=''
+     },
+    //  handleKeyup(e){
+    //    //logging event object
+    //    console.log(e)
+    //    if(e.keyCode === 27){//esc
+    //      this.clearMessage()
+    //    }else if (e.keyCode === 13){//enter
+    //      this.alertMessage()
+    //    }
+    //  },
+     alertMessage(){
+       alert(this.message)
+     }
+  }
 }
 </script>
