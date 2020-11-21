d43ae75e7200b1976d40968f16d23bd6b57a29d1
abeer <abeer@dataloop.ai>
2020-11-21 18:42:28 +0200

    footer

--- fabulous-todo/src/layouts/Layout.vue
+++ fabulous-todo/src/layouts/Layout.vue
@@ -1,41 +1,46 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR fFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
 
-        <q-toolbar-title>
-          Quasar App
+
+        <q-toolbar-title class="absolute-center">
+          Fabulous Todo App
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
+    <q-footer elevated>
+      <q-tabs>
+        <q-route-tab
+          v-for="nav in navs"
+          :icon="nav.icon"
+          :label="nav.label"
+          :to="nav.to"
+          :key="nav.label"
+        />
+      </q-tabs>
+    </q-footer>
+
     <q-drawer
       v-model="leftDrawerOpen"
+      :width="250"
+      :breakpoint="767"
       show-if-above
       bordered
-      content-class="bg-grey-1"
+      content-class="bg-primary"
     >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
+      <q-list dark>
+        <q-item-label header class="text-grey-8">
           Navigation
         </q-item-label>
         <EssentialLink
           v-for="link in essentialLinks"
           :key="link.title"
           v-bind="link"
+          class="text-grey-4"
         />
       </q-list>
     </q-drawer>
@@ -47,31 +52,50 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
+import EssentialLink from "components/EssentialLink.vue";
 
 const linksData = [
   {
-    title: 'Todo',
-    caption: 'My to-do list',
-    icon: 'list',
-    link: '/'
+    title: "Todo",
+    caption: "My to-do list",
+    icon: "list",
+    link: "/"
   },
-    {
-    title: 'Settings',
-    caption: 'My settings',
-    icon: 'settings',
-    link: '#/settings'
-  },
+  {
+    title: "Settings",
+    caption: "My settings",
+    icon: "settings",
+    link: "#/settings"
+  }
 ];
 
 export default {
-  name: 'MainLayout',
+  name: "MainLayout",
   components: { EssentialLink },
-  data () {
+  data() {
     return {
       leftDrawerOpen: false,
-      essentialLinks: linksData
-    }
+      essentialLinks: linksData,
+      navs: [
+        {
+          label: "Todo",
+          icon: "list",
+          to: "/"
+        },
+        {
+          label: "Settings",
+          icon: "settings",
+          to: "settings"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="scss">
+@media screen and (min-width: 768px) {
+  .q-footer {
+    display: none;
   }
 }
-</script>
+</style>
