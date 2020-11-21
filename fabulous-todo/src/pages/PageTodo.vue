1765d3644ac18353f04fb5e6bd9f57fed3f629d3
abeer <abeer@dataloop.ai>
2020-11-21 18:48:13 +0200

    add list of tasks

--- fabulous-todo/src/pages/PageTodo.vue
+++ fabulous-todo/src/pages/PageTodo.vue
@@ -7,7 +7,27 @@
 <script>
 
 export default {
-    
+  data() {
+    return {
+        tasks: [
+          {
+            id: 1,
+            name: 'Go to shop',
+            completed: false
+          },
+          {
+            id: 2,
+            name: 'Wash dishes',
+            completed: false
+          },
+          {
+            id: 3,
+            name: 'Watch TV',
+            completed: false
+          }
+        ]
+    }
+  }  
 }
 </script>
 
