1e4f7c9bbcc24835944f00b70f2b66143cb81ae1
abeer <abeer@dataloop.ai>
2020-11-21 19:04:22 +0200

    check list markup

--- fabulous-todo/src/pages/PageTodo.vue
+++ fabulous-todo/src/pages/PageTodo.vue
@@ -1,36 +1,51 @@
 <template>
   <q-page>
-  <p> To do page </p>
+    <q-list bordered>
+
+      <q-item 
+        v-for="task in tasks"
+        :hey="task.id"
+        v-ripple
+      >
+        <q-item-section side top>
+          <q-checkbox 
+            v-model="task.completed"
+          />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ task.name }}</q-item-label>
+        </q-item-section>
+      </q-item>
+      
+    </q-list>
   </q-page>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
-        tasks: [
-          {
-            id: 1,
-            name: 'Go to shop',
-            completed: false
-          },
-          {
-            id: 2,
-            name: 'Wash dishes',
-            completed: false
-          },
-          {
-            id: 3,
-            name: 'Watch TV',
-            completed: false
-          }
-        ]
-    }
-  }  
-}
+      tasks: [
+        {
+          id: 1,
+          name: "Go to shop",
+          completed: false
+        },
+        {
+          id: 2,
+          name: "Wash dishes",
+          completed: false
+        },
+        {
+          id: 3,
+          name: "Watch TV",
+          completed: false
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
